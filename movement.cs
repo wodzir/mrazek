6b5633f111445cc623a79cb10d453e7c37c5ffb6
aeger22 <36408883+aeger22@users.noreply.github.com>
2018-03-04 10:50:10 +0100

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    float x = 0;
+    
+   
+	// Update is called once per frame
+	void Update () {
+        //pohyb WASD
+        //D 
+        if (Input.GetKey("d"))
+        {
+            x = x + Time.deltaTime * 5;
+            transform.position = new Vector3(x, transform.position.y, 0);           
+        }
+        //A
+        if (Input.GetKey("a"))
+        {
+            x = x - Time.deltaTime * 5;
+            transform.position = new Vector3(x, transform.position.y, 0);      
+        }
+        //pobyb konec
+        //skok
+        if (Input.GetKeyDown("space"))
+        {
+            float vyskaSkoku = transform.position.y;
+            vyskaSkoku = vyskaSkoku + Time.deltaTime * 200;
+            transform.position = new Vector3(transform.position.x, vyskaSkoku, 0);
+        }         
+    }
+}
