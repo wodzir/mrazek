35e7bfd458e03fc2ae55a95c37a64a5a31c902ad
aeger22 <36408883+aeger22@users.noreply.github.com>
2018-03-06 20:45:59 +0100

    Add files via upload

---
+++ Movement3.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+	private Rigidbody2D rb2d;
+    float start;
+	// Use this for initialization
+	void Start () {
+        start = transform.position.y;
+        rb2d = GetComponent <Rigidbody2D>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        //pohyb WASD
+        //D 
+        if (Input.GetKey("d"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 3);
+        }
+        //A
+        if (Input.GetKey("a"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 3);
+        }
+        //pohyb šipky
+        //pravá šipka
+        if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 3);
+        }
+        //levá šipka
+        if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 3);
+        }
+        //pobyb konec
+
+        float y = 500f;
+        float now = transform.position.y;
+        //skok mezerník  
+        if ((int)now == (int)start)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                rb2d.AddForce(new Vector2(0f, y));
+            }
+        }
+        //skok šipka up  
+        if ((int)now == (int)start)
+        {
+            if (Input.GetKeyDown("up"))
+            {
+                rb2d.AddForce(new Vector2(0f, y));
+            }
+        }
+	}
+}
